80c74dc19614b3fcd25b511cad540bad84a6ffbd
javoski <xujiongbo@gmail.com>
2018-04-01 00:50:36 +0800

    remove debug codes

--- src/components/ASTExplorer.vue
+++ src/components/ASTExplorer.vue
@@ -15,9 +15,6 @@ export default {
       required: true
     }
   },
-  mounted () {
-    console.log(this.ast)
-  },
   components: {
     ASTNode
   }
--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -70,7 +70,5 @@ export default {
     }
   }
 }
-
-
 </style>
 
